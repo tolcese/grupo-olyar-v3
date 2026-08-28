85567b271b72773daefa6921da5f26f9a50bf82e
tolcese <tolcese@grupo-olyar.com>
2026-08-28 13:52:23 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'grupo-olyar-v3.7.5';
+const CACHE_NAME = 'grupo-olyar-v3.7.6';
 
 // Solo cacheamos iconos y manifest — el index.html siempre va a la red
 const STATIC_ASSETS = [
